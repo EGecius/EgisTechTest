46321a0ac0f7ed184f799e0ddbfece71e21ea627
Egidijus Gecius <e.gecius@gmail.com>
2019-03-25 13:59:26 +0000

    add test for Circle CI

---
+++ app/src/test/java/com/egecius/egisbabylontechtest/ListActivityTest.kt
@@ -0,0 +1,21 @@
+package com.egecius.egisbabylontechtest
+
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class ListActivityTest {
+
+    private var mSut: ListActivity? = null
+
+    @Before
+    fun setUp() {
+        mSut = ListActivity()
+    }
+
+    @Test
+    fun toTestCircleCi() {
+        assertTrue(false)
+    }
+
+}
