cc7d417f1300060d7e9dc28b57e142b29e420d27
Egidijus Gecius <e.gecius@gmail.com>
2019-03-25 14:30:50 +0000

    add PostsRetrofitAdapter

---
+++ app/src/main/java/com/egecius/egisbabylontechtest/PostsRetrofitAdapter.kt
@@ -0,0 +1,42 @@
+package com.egecius.egisbabylontechtest
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+class PostsRetrofitAdapter {
+
+    private val baseUrl = "http://jsonplaceholder.typicode.com/"
+
+    fun setupRetrofit(): PostsService {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(createLoggingOkHttpClient())
+            .build()
+            .create(PostsService::class.java)
+    }
+
+    private fun createLoggingOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
+
+}
+
+interface PostsService {
+
+    @GET("posts")
+    fun getPosts()
+
+    @GET("users")
+    fun getUsers()
+
+    @GET("comments")
+    fun getComments()
+}
