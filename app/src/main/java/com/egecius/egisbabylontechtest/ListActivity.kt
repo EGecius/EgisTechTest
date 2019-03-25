7ac41d268a6a655e4bfcf8608cdb1e5ee947b08a
Egidijus Gecius <e.gecius@gmail.com>
2019-03-25 13:42:48 +0000

    add temporary title

--- app/src/main/java/com/egecius/egisbabylontechtest/ListActivity.kt
+++ app/src/main/java/com/egecius/egisbabylontechtest/ListActivity.kt
@@ -2,12 +2,15 @@ package com.egecius.egisbabylontechtest
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_list.*
 
 class ListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        egis_title.text = "Babylon Tech test"
     }
 
 }
