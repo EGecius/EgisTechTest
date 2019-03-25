1f4d25553fcc5842ea5b18123ecf893090743813
Egidijus Gecius <e.gecius@gmail.com>
2019-03-25 13:51:32 +0000

    use AndroidX AppCompatActivity

--- app/src/main/java/com/egecius/egisbabylontechtest/ListActivity.kt
+++ app/src/main/java/com/egecius/egisbabylontechtest/ListActivity.kt
@@ -1,7 +1,7 @@
 package com.egecius.egisbabylontechtest
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_list.*
 
 class ListActivity : AppCompatActivity() {
