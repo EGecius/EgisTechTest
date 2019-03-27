36e57bc9017b39cc8e2ad3e80a0c043e8e2fc20d
Egidijus Gecius <e.gecius@gmail.com>
2019-03-27 11:38:57 +0000

    simplify CommentsMapper

--- app/src/main/java/com/egecius/egisbabylontechtest/features/showpostdetail/comments/CommentsMapper.kt
+++ app/src/main/java/com/egecius/egisbabylontechtest/features/showpostdetail/comments/CommentsMapper.kt
@@ -1,15 +1,13 @@
 package com.egecius.egisbabylontechtest.features.showpostdetail.comments
 
+import com.annimon.stream.Stream
+
 class CommentsMapper {
 
     fun toComments(commentsJson: List<CommentJson>): List<Comment> {
 
-        val resultList = mutableListOf<Comment>()
-
-        for (commentJson in commentsJson) {
-            resultList.add(Comment(commentJson.id, commentJson.postId, commentJson.name, commentJson.body))
-        }
-
-        return resultList
+        return Stream.of(commentsJson)
+            .map { Comment(it.id, it.postId, it.name, it.body) }
+            .toList()
     }
 }
