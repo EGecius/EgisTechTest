61997721fa72bc047af6d5fe2b9d76b29f0530d3
Egidijus Gecius <e.gecius@gmail.com>
2019-03-28 13:58:15 +0000

    annotate Post for Room

--- app/src/main/java/com/egecius/egisbabylontechtest/features/showpostlist/post/Post.kt
+++ app/src/main/java/com/egecius/egisbabylontechtest/features/showpostlist/post/Post.kt
@@ -2,8 +2,13 @@ package com.egecius.egisbabylontechtest.features.showpostlist.post
 
 import android.os.Parcel
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-data class Post(val id: Int, val title: String, val body: String, val userId: Int) : Parcelable {
+@Entity(tableName = "posts")
+data class Post(
+    @PrimaryKey
+    val id: Int, val title: String, val body: String, val userId: Int) : Parcelable {
     // this depends on Java's inability to guarantee non-null
     @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
     constructor(parcel: Parcel) : this(
